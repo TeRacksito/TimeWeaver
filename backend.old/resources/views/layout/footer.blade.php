20cf7c5848964458ebd56912f071d8c84eed4f2f
TeRacksito <acho.chuchi.8@gmail.com>
2025-03-26 11:08:32 +0000

    remove old backend

--- backend.old/resources/views/layout/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="container mx-auto px-6 py-8 text-center text-slate-500">
-  <p class="animate-fadeIn delay-600">
-    <span class="text-slate-400">{{ config('app.name') }}</span> • Development Environment • {{ date('Y') }}
-  </p>
-</footer>
